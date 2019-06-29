2e7d3d6c5e320c5ea24e1fb615166631408a2e3c
kanyuanzhi <kan@mail.ustc.edu.cn>
2019-06-29 13:25:09 +0800

    update

---
+++ src/model_validation.py
@@ -0,0 +1,85 @@
+from scipy.integrate import quad
+from scipy.optimize import fsolve
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+from mcav.zipf import Zipf
+from che import Che
+from math import exp
+
+
+class ValidationUniform(object):
+    def __init__(self, amount, total_rate, expected_value, popularity):
+        self._amount = amount
+        self._popularity = popularity
+        self._total_rate = total_rate
+        self._ev = expected_value
+        self._rate = self._requestRate()
+        self._validation_ratio = self._validationRatio()
+    
+    def _requestRate(self):
+        rr = {}
+        for i in range(1, self._amount+1):
+            rr[i] = self._total_rate * self._popularity[i]
+        return rr
+    
+    def _validationRatio(self):
+        ev = self._ev
+        validation_ratio = {}
+        for i in range(1, self._amount+1):
+            rate = self._rate[i]
+            validation_ratio[i] = quad(self._F(rate, ev).F1, 0, 2 * ev)[0]
+        return validation_ratio
+
+    def validationRatio(self):
+        return self._validation_ratio
+
+    def totalValidationRatio(self):
+        thr = 0
+        for i in range(1, self._amount+1):
+            thr = thr + self._popularity[i]*self._validation_ratio[i]
+        return thr
+
+    class _F(object):
+        def __init__(self, rate, ev):
+            self._rate = rate
+            self._ev = ev
+
+        def F1(self, Tv):
+            return (Tv + exp(-self._rate * Tv)/self._rate - 1/self._rate) / (2 * self._ev * self._ev)
+
+
+if __name__ == "__main__":
+    amount = 5000
+    z = 0.8
+    cachesize = 50
+    total_rate = 20
+    expected_value = 20
+
+    zipf = Zipf(amount, z)
+    popularity = zipf.popularity()
+
+    print(ValidationUniform(amount,total_rate,expected_value,popularity).totalValidationRatio())
+
+    
+    # index = []
+    # result = []
+    # for expected_value in range(4, 41):
+    #     index.append(expected_value)
+
+    #     validation = ValidationUniform(amount,total_rate,expected_value,popularity)
+    #     result.append(validation.totalValidationRatio())
+
+    #     print(expected_value)
+
+
+    # plt.plot(index, result, "+-", label="simulation")
+    # plt.xlabel("content ID")
+    # plt.ylabel("validation ratio")
+    # plt.grid(True)
+    # # plt.axis([0, 51, 0, 1])
+    # plt.legend()
+    # plt.show()
+    
+    
+   
